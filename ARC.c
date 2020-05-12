0def83d4414283e5bdd959cd9bb024df177718be
SergeyKoshelev <Koshelev.SM@phystech.edu>
2020-05-12 21:41:05 +0300

    some changes

--- ARC.c
+++ ARC.c
@@ -2,15 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-
-
-/*
-now we need to
-1)connect parts of cache with initialization function of list (function initialization)
-2)connect with function of getting page (function get_page)
-3)create function of finding element in parts (function find)
-4)get informmation about cache part size (maybe new function cachesize)
-*/
+#include "pages.h"
 
 
 void replace(p)
@@ -27,34 +19,48 @@ void replace(p)
 	//}
 }
 
+int find(long long int page_name, struct cell* cache_part)
+{
+	if (find_cell(cache_part, page_name) != NULL)
+		return 1;
+	else
+		return 0;
+}
+
+long long int fast_get_page()    //input one page
+{
+	return 0;
+}
+
 void Cache(int c, int pages_count)			//main function of ARC
 {									//c is parameter of cache size
 									//pages_count is count of pages
 
 	//printf("Start caching\n");
 	int p = 0;
-	struct page x;
-	struct list T1 = initialization(...);
-	struct list T2 = initialization(...);               //initialization of cache parts
-	struct list B1 = initialization(...);
-	struct list B2 = initialization(...);
+	long long int page_name;
+	int len_T1 = 0, len_T2 = 0, len_T3 = 0, len_B2 = 0;  //
+	struct cell* T1 = make_list(0);
+	struct cell* T2 = make_list(0);            //initialization of cache parts
+	struct cell* B1 = make_list(0);
+	struct cell* B2 = make_list(0);
 	
 	for (int i = 0; i < pages_count; i++)				//loop of cache working
 	{
-		x = get_page();
+		page_name = fast_get_page();
 
-		if ((find(x, &T1) || (find(x, &T2))				//case 1
+		if (find(page_name, T1) || find(page_name, T2))				//case 1
 		{
 			//move x to the top of T2
 		}
-		else if (find(x, &B1))							//case 2
+		else if (find(page_name, B1))							//case 2
 		{
 			//p = min(c, p + max(cachesize(B2)/cachesize(B1), 1);
 			//replace(p);
 			//move x to the top of T2
 			//place x into the cache (???)
 		}
-		else if (find(x, &B2))							//case 3
+		else if (find(page_name, B2))							//case 3
 		{
 			//p = max(0, p - max(cachesize(B1)/cachesize(B2), 1);
 			//replace(p);
@@ -74,7 +80,7 @@ void Cache(int c, int pages_count)			//main function of ARC
 					//delete LRU page of T1
 					//remove it from the cache
 				}
-			if ((cachesize(L1) < c) && ((cashesize(L1) + cashesize(L2)) >= c))
+			if ((cachesize(L1) < c) && ((cañhesize(L1) + cañhesize(L2)) >= c))
 				if ((cashesize(L1) + cashesize(L2)) == 2 * c)
 				{
 					//delete the LRU page of B2
@@ -85,5 +91,9 @@ void Cache(int c, int pages_count)			//main function of ARC
 		}
 	}
 
+	destroy_list(T1);
+	destroy_list(B1);
+	destroy_list(T2);
+	destroy_list(B2);
 	//printf("Stop caching");
 }

feff7855b7aba9461a094d477edf86efa6f9fe0f
SergeyKoshelev <Koshelev.SM@phystech.edu>
2020-05-13 13:55:20 +0300

    only putting in cache remain

--- ARC.c
+++ ARC.c
@@ -6,94 +6,151 @@
 
 const int c = 10;
 
-struct list {
+struct list_t {
 	struct cell* head;
-	int size;
+	unsigned long long length;
+	struct cell* end;
+	struct cell* last_found;
 };
 
-void replace(int *p, long long int page_name, struct list* T1, struct list* T2, struct list* B1, struct list* B2)
+struct cell {
+	struct cell* prev;
+	struct cell* next;
+	long long int data;
+	struct page_t* page_ptr;
+};
+
+void replace(int *p, long long int page_name, struct list_t* T1, struct list_t* T2, struct list_t* B1, struct list_t* B2)
 {
+	/*
+	if ((cachesize(T1) >=1) && ((find(x, B2) && (cachesize(T1) == p)) || (cachesize(T1) >= p)))
+	/move LRU page of T1 to the bottom of B1
+	remove it from the cache
+	else
+	move LRU page of T2 to the bottom of B2
+	remove it from the cache
+	*/
 	struct cell* page_in_B2 = find_cell(B2, page_name);
-	if (((T1->size) >=1) && (((page_in_B2 != NULL) && ((T1->size) == (*p))) || ((T1->size) >= (*p))))
+	if (((T1->length) >=1) && (((page_in_B2 != NULL) && ((T1->length) == (*p))) || ((T1->length) >= (*p))))
 	{
-	//move LRU page of T1 to the bottom of B1
-	//remove it from the cache
+		/*
+		move LRU page of T1 to the top of B1
+		remove it from the cache
+		*/
+		replace_lf_to_head(T1, B1, NULL);
+		//remove it from the cache
 	}
 	else
 	{
-	//move LRU page of T2 to the bottom of B2
-	//remove it from the cache
+		/*
+		move LRU page of T2 to the top of B2
+		remove it from the cache
+		*/
+		replace_lf_to_head(T2, B2, NULL);
+		//remove it from the cache
 	}
 }
 
-struct cell* fast_get_page(long long int page_name, struct list* T1, struct list* T2, struct list* B1, struct list* B2, int *p)			//main function of ARC
+struct cell* insert_in_head(struct list_t* part, long long int page_name, struct page_t* page_ptr)
+{
+	struct cell* new_head = (struct cell*)calloc(1, sizeof(struct cell));
+	assert(new_head == NULL);
+	new_head->data = page_name;
+	new_head->page_ptr = page_ptr;
+	return insert_to_head(part, new_head);
+}
+
+struct cell* fast_get_page(long long int page_name, struct list_t* T1, struct list_t* T2, struct list_t* B1, struct list_t* B2, int *p)			//main function of ARC
 {									
-
-	//int p = 0;
-	//long long int page_name;
-	//struct cell* T1 = make_list(0);
-	//struct cell* T2 = make_list(0);            //initialization of cache parts
-	//struct cell* B1 = make_list(0);
-	//struct cell* B2 = make_list(0);
-
+	/*
+	int p = 0;
+	long long int page_name;
+	struct cell* T1 = make_list(0);
+	struct cell* T2 = make_list(0);            //initialization of cache parts
+	struct cell* B1 = make_list(0);
+	struct cell* B2 = make_list(0);
+	*/
 	struct cell* page_in_T1 = find_cell(T1, page_name);
 	struct cell* page_in_T2 = find_cell(T2, page_name);
 	if ((page_in_T1 != NULL) || (page_in_T2 != NULL))				//case 1
 	{
-		//move x to the top of T2
+		/*
+		move x to the top of T2
+		*/
 		if (page_in_T1 != NULL)
-		{
-			destroy_cell(page_in_T1);
-			T1->size--;
-		}
+			return replace_lf_to_head(T1, T2, page_in_T1);
 		if (page_in_T2 != NULL)
-			destroy_cell(page_in_T2);
-		//function of moving x to the top of T2
-		return NULL;	//return current page
+			return replace_lf_to_head(T2, T2, page_in_T2);
 	}
 
 	struct cell* page_in_B1 = find_cell(B1, page_name);
 	if (page_in_B1 != NULL)							//case 2
 	{
-		*p = min(c, p + max((B2->size) / (B1->size), 1));
+		/*
+		p = min(c, p + max(cachesize(B2)/cachesize(B1), 1);
+		replace(p);
+		move x to the top of T2
+		place x into the cache
+		*/
+		*p = min(c, p + max((B2->length) / (B1->length), 1));
 		replace(p, page_name, T1, T2, B1, B2);
-		//function of moving x to the top of T2
-		//place x into the cache (???)
-		return NULL;	//return current page
+		replace_lf_to_head(B1, T2, page_in_B1);
+		//place x into the cache
+		return T2->head;
 	}
 
 	struct cell* page_in_B2 = find_cell(B2, page_name);
 	if (page_in_B2 != NULL)								//case 3
 	{
-		*p = max(0, p - max((B1->size) / (B2->size), 1));
+		/*
+		p = max(0, p - max(cachesize(B1)/cachesize(B2), 1);
+		replace(p);
+		move x to the top of T2
+		place x into the cache
+		*/
+		*p = max(0, p - max((B1->length) / (B2->length), 1));
 		replace(p, page_name, T1, T2, B1, B2);
-		//function of moving x to the top of T2
-		//place x into the cache (???)
-		return NULL;	//return current page
+		replace_lf_to_head(B2, T2, page_in_B2);
+		//place x into the cache 
+		return T2->head;
 	}
 
 
-	int L1_size = (B1->size) + (T1->size);
-	int L2_size = (B2->size) + (T2->size);
-	if (L1_size == c)						//case 4.1
-		if (((T1->size)) < c)
+	int L1_length = (B1->length) + (T1->length);
+	int L2_length = (B2->length) + (T2->length);
+	if (L1_length == c)						//case 4.1
+		if (((T1->length)) < c)
 		{
-			//delete LRU page of B1
+			/*
+			delete LRU page of B1
+			replace(p)
+			*/
+			delete_last_elem(B1);
 			replace(p, page_name, T1, T2, B1, B2);
 		}
 		else
 		{
-			//delete LRU page of T1
-			//remove it from the cache
+			/*
+			delete LRU page of T1
+			remove it from the cache
+			*/
 		}			
-	else if ((L1_size < c) && ((L1_size + L2_size) >= c))  //case 4.2
-		if ((L1_size + L2_size) == (2 * c))
+	else if ((L1_length < c) && ((L1_length + L2_length) >= c))  //case 4.2
+		if ((L1_length + L2_length) == (2 * c))
 		{
-			//delete the LRU page of B2
+			/*
+			delete the LRU page of B2
+			replace(p)
+			*/
+			delete_last_elem(B2);
 			replace(p, page_name, T1, T2, B1, B2);
-		}		
-		find_mem(page_name);
-		//put x at the top of T1
-		//place it into the cache
-		return NULL;	//return current page
+		}
+	/*
+	put x at the top of T1
+	place it into the cache
+	*/
+	//page_t* page_ptr = find_mem(page_name);
+	insert_in_head(T1, page_name, page_ptr);
+	//place it into the cache
+	return T1->head;
 }

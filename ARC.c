1f3fefef23c679bcaaba2030042442adadc1ed2a
SergeyKoshelev <Koshelev.SM@phystech.edu>
2020-05-16 13:58:29 +0300

    asserts, instructions and header of arc.c

--- ARC.c
+++ ARC.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -9,54 +8,85 @@
 #include "ARC.h"
 #include "conditions.h"
 
-long long int imax (long long int a, long long int b)   //simply max
+/*
+Small Instruction:
+imax - function of finding max long long int element from given two
+imin - function of finding min long long int element from given two
+replace - function of replacing elements in cache due to some conditions (mostly due to parameter p and cache sizes
+insert_in_head - function insert new cell in given part of cache with given cache_mem pointer and page_name
+from_mem_to_cache_mem - copying page with given page_name from memory to cache_memory
+fast_get_page - main function of ARC, it uses ARC algorithm to cache given page 
+*/
+
+long long int imax (long long int a, long long int b)   
 {
+	//simply max
 	if (a >=b) return a;
 	else return b;
 }
 
-long long int imin(long long int a, long long int b)	//simply min
+long long int imin(long long int a, long long int b)	
 {
+	//simply min
 	if (a <=b) return a;
 	else return b;
 }
 
-
-void replace(unsigned long long *p, long long int page_name, struct list_t* T1, struct list_t* T2, struct list_t* B1, struct list_t* B2, struct page_t* mem, struct cache_t* cache_mem)
+void replace(unsigned long long *p, 
+			long long int page_name, 
+			struct list_t* T1, 
+			struct list_t* T2, 
+			struct list_t* B1, 
+			struct list_t* B2)
 {
+	assert((p >= 0) && (p <= CACHE_SIZE) && "Incorrect parameter p in replace");
+	assert(p, T1 && T2 && B1 && B2 && "Get null pointer in replace");
+	
+
+	//here decision is made due to param p: should program put the oldest of T1 or T2 to the ghost part of cache
 	struct cell* page_in_B2 = find_list_elem(B2, page_name);
-	if (((T1->length) >=1) && (((page_in_B2 != NULL) && ((T1->length) == (*p))) || ((T1->length) >= (*p))))
-	{												//(if T1 is not empty) and <{(we found page in B2) and (length_T1 == p)}  or (length_T1 >= p)>
-													//oldest of T1 -> newest in B1, and delete from cache_memory
+	if (((T1->length) >=1) && ((page_in_B2 && ((T1->length) == (*p))) || ((T1->length) >= (*p))))
+	{												//if program should chose the oldest in T1
 		/*
 		move LRU page of T1 to the top of B1
-		remove it from the cache
+		remove this page from the cache
 		*/
-		replace_lf_to_head(T1, B1, T1->end);
+		struct cell* B1_head = replace_lf_to_head(T1, B1, T1->end);
+		assert((B1_head == B1->head) && (B1_head != NULL) && "error with replace_lf_to_head in replace_B1");
 		struct cache_t* temp = cell_cache(B1->head);
+		assert((temp != NULL) && (temp->flag == 1) && "cell_cache return false pointer on cache_mem in replace_B1");
 		temp->flag = 0;
 	}
-	else											//else
-	{												//oldest of T2 -> newest in B2, and delete from cache_memory
+	else											
+	{												//else (it should take the oldest in T2)
 		/*
 		move LRU page of T2 to the top of B2
-		remove it from the cache
+		remove this page from the cache
 		*/
-		replace_lf_to_head(T2, B2, T2->end);
+		struct cell* B2_head = replace_lf_to_head(T2, B2, T2->end);
+		assert((B2_head == B2->head) && (B2_head != NULL) && "error with replace_lf_to_head in replace_B2");
 		struct cache_t* temp = cell_cache(B2->head);
+		assert((temp != NULL) && (temp->flag == 1) && "cell_cache return false pointer on cache_mem in replace_B2");
 		temp->flag = 0;
 	}
 }
 
 struct cell* insert_in_head(struct list_t* part, long long int page_name, struct cache_t* cache_ptr)
 {
-	//insert new cell in this part with these cache_ptr and page_name
+	assert(part && cache_ptr && "Get null pointer in insert_in_head");
+
+	//creating new cell with give page_name and pointer on cache mem, then put it on top of cache_part
 	struct cell* new_head = make_cell_np(page_name, cache_ptr);
-	return insert_to_head(part, new_head);
+	assert(new_head && "make_cell_np return NULL in insert_in_head");
+	struct cell* part_head = insert_to_head(part, new_head);
+	assert((part_head == part->head) && "wrong insert in head");
+	return part_head;
 }
 
 struct cache_t* from_mem_to_cache_mem(long long int page_name, struct page_t* mem, struct cache_t* cache_mem)
 {
+	assert(mem && cache_mem && "Get null pointer in from_mem_to_cache_mem");
+
 	#ifdef PRINT_REQ
 	printf("CACHE_MISS!\n");
 	#endif
@@ -65,112 +95,139 @@ struct cache_t* from_mem_to_cache_mem(long long int page_name, struct page_t* me
 	my_delay(MEM_DELAY);
 	#endif
 	
-	//copying page with page_name from mem to cache_mem
 	int i = 0;
-	struct page_t* page_in_mem = find_page(page_name, mem);   //pointer on page in mem with this page_name
+	struct page_t* page_in_mem = find_page(page_name, mem);   //try to find pointer on page in memory with this page_name
 	assert(page_in_mem && "No such page in memory");
-	for (i = 0; i < CACHE_SIZE; i++)				//trying to find free space in cache_mem
-		if (cache_mem[i].flag == 0)
+	for (i = 0; i < CACHE_SIZE; i++)				//try to find free space in cache_memory
+		if (cache_mem[i].flag == 0)					
 		{
-			//if we found, place there new page by copying it from memory
+			//if we found, place there new page by copying it from memory and turn on flag, that this cell is not empty
 			cache_mem[i].flag = 1;
 			memcpy(&cache_mem[i].page, page_in_mem, sizeof(struct page_t));
 			assert(((cache_mem + i) != NULL) && "Mem to cache");
 			return (cache_mem + i);
 		}
-	exit(666);
+	assert((i < CACHE_SIZE) && "No free space in cache_mem");
 }
 
-struct cell* fast_get_page(unsigned long long* p, long long int page_name, struct list_t* T1, struct list_t* T2, struct list_t* B1, struct list_t* B2, struct page_t* mem, struct cache_t* cache_mem, int* T_hits)			//main function of ARC
+struct cell* fast_get_page(unsigned long long* p, 
+						long long int page_name, 
+						struct list_t* T1, 
+						struct list_t* T2, 
+						struct list_t* B1, 
+						struct list_t* B2, 
+						struct page_t* mem, 
+						struct cache_t* cache_mem, 
+						int* T_hits)
 {		
+	assert(p && T1 && T2 && B1 && B2 && mem && cache_mem && T_hits && "Get null pointer in fast_get_page");
+	assert(((T1->length + T2->length) <= CACHE_SIZE) && "Too big real cache");
+
 	#ifdef DELAY
 	my_delay(CACHE_DELAY);
 	#endif
 
+
+	//case 1: hit in T1 or T2
 	struct cell* page_in_T1 = find_list_elem(T1, page_name);
 	struct cell* page_in_T2 = find_list_elem(T2, page_name);
-
-	if ((page_in_T1 != NULL) || (page_in_T2 != NULL))				//case 1: hit in T1 or T2
-	{																//put on top of T2
+	if ((page_in_T1 != NULL) || (page_in_T2 != NULL))				
+	{															
 		/*
-		move x to the top of T2
+		move this page to the top of T2
 		*/
 		(*T_hits)++;
-		//printf("HIT!\n");
 		if (page_in_T1 != NULL)
-			return replace_lf_to_head(T1, T2, page_in_T1);
-		if (page_in_T2 != NULL)
-			return replace_lf_to_head(T2, T2, page_in_T2);
+		{
+			struct cell* T2_head = replace_lf_to_head(T1, T2, page_in_T1);
+			assert(T2_head && (T2_head == T2->head) && "Case 1, hit in T1");
+			return T2_head;
+		}
+		else
+		{
+			struct cell* T2_head = replace_lf_to_head(T2, T2, page_in_T2);
+			assert(T2_head && (T2_head == T2->head) && "Case 1, hit in T2");
+			return T2_head;
+		}
 	}
 
-	struct cell* page_in_B1 = find_list_elem(B1, page_name);
 
-	if (page_in_B1 != NULL)											//case 2: hit in B1 
-	{																//change p, replace smth, put page from B1 to T2 and put it in cache_mem
+	//case 2: hit in B1 
+	struct cell* page_in_B1 = find_list_elem(B1, page_name);
+	if (page_in_B1 != NULL)											
+	{																
 		/*
-		p = min(c, p + max(cachesize(B2)/cachesize(B1), 1);
-		replace(p);
-		move x to the top of T2
-		place x into the cache
+		change p using equation: p = min(c, p + max(cachesize(B2)/cachesize(B1), 1);
+		replace smth in cache using function <replace>;
+		then put found page to the top of T2
+		and place it into the cache
 		*/
 		*p = imin(CACHE_SIZE, *p + imax((B2->length) / (B1->length), 1));
 		replace(p, page_name, T1, T2, B1, B2, mem, cache_mem);
-		replace_lf_to_head(B1, T2, page_in_B1);
+		struct cell* T2_head = replace_lf_to_head(B1, T2, page_in_B1);
+		assert((T2_head == T2->head) && "Case 2, insert in head");
 		struct cache_t* temp = from_mem_to_cache_mem(page_name, mem, cache_mem);
-		assert(temp != NULL && "Page B1");
+		assert(temp && "Case 2, copy to cache_mem");
 		set_cache(T2->head, temp);
 		return T2->head;
 	}
 
-	struct cell* page_in_B2 = find_list_elem(B2, page_name);
 
-	if (page_in_B2 != NULL)											//case 3: hit in B2
-	{																//change p, replace smth, put page from B2 to T2 and put it in cache_mem
+	//case 3: hit in B2
+	struct cell* page_in_B2 = find_list_elem(B2, page_name);
+	if (page_in_B2 != NULL)											
+	{									
 		/*
-		p = max(0, p - max(cachesize(B1)/cachesize(B2), 1);
-		replace(p);
-		move x to the top of T2
-		place x into the cache
+		change p using equation: p = max(0, p - max(cachesize(B1)/cachesize(B2), 1);
+		replace smth in cache using function <replace>;
+		then put found page to the top of T2
+		and place it into the cache
 		*/
 		*p = imax(0, *p - imax((B1->length) / (B2->length), 1));
 		replace(p, page_name, T1, T2, B1, B2, mem, cache_mem);
-		replace_lf_to_head(B2, T2, page_in_B2);
+		struct cell* T2_head = replace_lf_to_head(B2, T2, page_in_B2);
+		assert((T2_head == T2->head) && "Case 3, insert in head");
 		struct cache_t* temp = from_mem_to_cache_mem(page_name, mem, cache_mem);
-		assert(temp != NULL && "Page B2");
+		assert(temp && "Case 3, copy to cache_mem");
 		set_cache(T2->head, temp);
 		return T2->head;
 	}
 
 
+	//case 4: new page
 	unsigned long long L1_length = (B1->length) + (T1->length);						//L1 and L2 sizes
 	unsigned long long L2_length = (B2->length) + (T2->length);
-	if (L1_length == CACHE_SIZE)			//case 4.1: L1 is full
-		if (((T1->length)) < CACHE_SIZE)								//if T1 is still not full
-		{																//delete oldest in B1 and change smth(oldest in T1 -> newest in B1)
+	assert(((L1_length + L2_length) <= 2 * CACHE_SIZE) && "Too big cache");
+
+	//case 4.1: L1 is full
+	if (L1_length == CACHE_SIZE)			
+		if (((T1->length)) < CACHE_SIZE)					//if T1 is still not full
+		{															
 			/*
 			delete LRU page of B1
-			replace(p)
+			replace smth using <replace>
 			*/
 			delete_last_elem(B1);
 			replace(p, page_name, T1, T2, B1, B2, mem, cache_mem);
 		}
-		else															//if T1 if full
-			{															//delete oldest in T1 and delete its mem
+		else											
+			{												//if T1 if full			
 			/*
 			delete LRU page of T1
 			remove it from the cache
 			*/
 			struct cache_t* temp = cell_cache(T1->end);
+			assert(temp && (temp->flag == 1) && "Case 4.1, wrong pointer on cache memory");
 			temp->flag = 0;
 			delete_last_elem(T1);
 		}			
-	else if ((L1_length < CACHE_SIZE) && ((L1_length + L2_length) >= CACHE_SIZE))		//case 4.2: if L1 is not full, but together L1+L2 more than cache_size
-	{																	//if L1+L2 full exactly (= 2*cache_size), delete oldest in B2
-																		//but ALWAYS change smth
+	//case 4.2: if L1 is not full, but together L1+L2 more than cache_size
+	else if ((L1_length < CACHE_SIZE) && ((L1_length + L2_length) >= CACHE_SIZE))
+	{														
 		/*
-		if ()
+		if L1+L2 full exactly (= 2*cache_size),
 			delete the LRU page of B2
-		replace(p)
+		ALWAYS: replace smth using <replace>
 		*/
 		if ((L1_length + L2_length) == (2 * CACHE_SIZE))
 			delete_last_elem(B2);
@@ -178,11 +235,12 @@ struct cell* fast_get_page(unsigned long long* p, long long int page_name, struc
 		replace(p, page_name, T1, T2, B1, B2, mem, cache_mem);
 		}
 	/*
-	put x at the top of T1
+	put new page at the top of T1
 	place it into the cache
 	*/
-	struct cache_t* temp = from_mem_to_cache_mem(page_name, mem, cache_mem);			//case: no hit, so create new and put it in mem and on top of T1
-	assert(temp != NULL && "Case 4.2");
-	insert_in_head(T1, page_name, temp);
+	struct cache_t* temp = from_mem_to_cache_mem(page_name, mem, cache_mem);
+	assert(temp && "Case 4, copying from memory");
+	struct cell* T1_head = insert_in_head(T1, page_name, temp);
+	assert(T1_head && (T1->head == T1_head) && "Case 4, wrong insert in head");
 	return T1->head;
 }
